c465262079cda04bdf66ed6e514db9fb7136b812
peyronn <peyronn>
2020-12-20 12:28:25 +0100

    start to implement youtube API

--- js/search.js
+++ js/search.js
@@ -1,3 +1,22 @@
-function search(){
-    console.log("pass");
+
+function init(){
+    gapi.client.setApiKey("AIzaSyA4doBzIwbcqZLK8y54cB9mjV8gPafDiXQ");
+    gapi.client.load("youtube", "v3", function(){
+        console.log("ready");
+        
+    });
+}
+
+function research(){
+    var request = gapi.client.youtube.search.list({
+        part: "snippet",
+        type: "video",
+        q: encodeURIComponent(document.getElementById("bar").value).replace(/%20/g, "+"),
+        maxResults: 10,
+        order: "viewCount"
+    });
+    request.execute(function(response){
+        var results = response;
+        console.log(results);
+    })
 }
--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const {app, BrowserWindow} = require("electron")
 
 function createWindow(){
     const win = new BrowserWindow({
-        width: 800,
-        height:  600,
+        width: 1400,
+        height:  1200,
         webPreferences: {
             nodeIntegration: true,
             devTools: true,
