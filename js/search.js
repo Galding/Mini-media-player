8aad9bd8300fc7d7272c3cb537360a555cbf1461
Galding <nathan.peyronnet@gmail.com>
2020-12-21 16:30:15 +0100

    add images for play and pause button and finish the play / pause system

--- js/search.js
+++ js/search.js
@@ -21,18 +21,18 @@ function research(){
 }*///AIzaSyA4doBzIwbcqZLK8y54cB9mjV8gPafDiXQ
 
 
-function research2(){
+function research(){
     var xhr = new XMLHttpRequest();
     var text = document.getElementById("bar").value;
     var query = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&order=viewCount&key=AIzaSyA4doBzIwbcqZLK8y54cB9mjV8gPafDiXQ&q="+text.replace(/%20/g, "+");
     xhr.open("GET", query, false);
     xhr.send(null);
     var res = JSON.parse(xhr.responseText);
-    console.log(res);
+    //console.log(res);
     return res;
 }
 function main(){
-    var results = research2();
+    var results = research();
     console.log(results.items);
     var div = document.getElementById("results");
     for(item of results.items){
@@ -44,7 +44,8 @@ function main(){
         playButton.setAttribute("type", "image");
         playButton.setAttribute("src", "img/playButton.png");
         playButton.setAttribute("alt", "play");
-        playButton.setAttribute("id", "button");
+        playButton.setAttribute("class", "button");
+        playButton.setAttribute("id", "pause");
         playButton.setAttribute("onclick", "javascript:play(this)");
         p.appendChild(playButton);
         div.appendChild(p);
@@ -52,9 +53,17 @@ function main(){
 }
 
 function play(selected){
+    var allButtons = selected.parentNode.parentNode.childNodes;
+    for(let i = 1; i < allButtons.length; i++){
+        var currentButton = allButtons[i].firstElementChild;
+        if(currentButton.id == "play"){
+            pause(currentButton);
+        }
+    }
     selected.setAttribute("src", "img/pauseButton.png");
     selected.setAttribute("onclick", "javascript:pause(this)");
     selected.setAttribute("alt", "pause");
+    selected.setAttribute("id", "play");
     var videoId = selected.id;
 }
 
@@ -62,6 +71,7 @@ function pause(selected){
     selected.setAttribute("src", "img/playButton.png");
     selected.setAttribute("onclick", "javascript:play(this)");
     selected.setAttribute("alt", "play");
+    selected.setAttribute("id", "pause");
 }
 /*
 var pauseButton = document.createElement("input");
