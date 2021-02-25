d46cce25e6c11a8cf7ed3ea159050b4157b1bd6a
Galding <nathan.peyronnet@gmail.com>
2021-02-25 15:46:46 +0100

    small fixes

--- js/search.js
+++ js/search.js
@@ -17,13 +17,12 @@ function research(){
         var results = response;
         console.log(results);
     });
-}*/ //AIzaSyA4doBzIwbcqZLK8y54cB9mjV8gPafDiXQ
-
-
+}*/ 
+var key = openFile("file:///"+__dirname + "/TOKEN.txt");
 function research() {
     var xhr = new XMLHttpRequest();
     var text = document.getElementById("bar").value;
-    var query = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&order=viewCount&key=AIzaSyA4doBzIwbcqZLK8y54cB9mjV8gPafDiXQ&q=" + text.replace(/%20/g, "+");
+    var query = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&order=viewCount&key=" + key +"&q=" + text.replace(/%20/g, "+");
     xhr.open("GET", query, false);
     xhr.send(null);
     var res = JSON.parse(xhr.responseText);
@@ -151,4 +150,21 @@ function emptyCue(){
         console.log("cue is now empty");
     }
     
+}
+
+
+function openFile(file){
+    var rawFile = new XMLHttpRequest();
+    var text;
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function (){
+        if(rawFile.readyState == 4){
+            if(rawFile.status === 200 || rawFile.status == 0){
+                text =  rawFile.responseText;
+                
+            }
+        }
+    }
+    rawFile.send(null);
+    return text;
 }
